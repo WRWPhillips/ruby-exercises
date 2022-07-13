ce69b044ef1df88171796402cb33a65464aff0a5
WRWPhillips <42WPhillips42@gmail.com>
2022-07-12 20:44:47 -0500

    Only one project left! Looking good

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -1,3 +1,5 @@
+require 'pry-byebug'
+
 # First, we're going to practice reading the Stack Trace
 # Don't look at this method prior to running the test
 # Type 'rspec' into the terminal to run Rspec
@@ -51,6 +53,7 @@ end
 
 def yell_greeting(string)
   name = string
+  binding.pry
   name = name.downcase
   greeting = "WASSAP, #{name}!"
 end
